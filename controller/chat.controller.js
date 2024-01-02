994cd88730e5dbf95589b60a6d302cc71db7345a
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2024-01-02 16:29:23 +0200

    add route get rooms

--- controller/chat.controller.js
+++ controller/chat.controller.js
@@ -2,6 +2,17 @@ import asyncHandler from 'express-async-handler';
 import { StatusCodes } from 'http-status-codes';
 import Chat from '../models/Chat.js';
 
+
+
+
+
+// Remove duplicates based on the "room" property
+const removeDuplicatedRoom = (arr) =>
+  arr.filter(
+    (object, index, self) =>
+      index === self.findIndex((room) => room.room === object.room)
+  );
+
 export const getChat = asyncHandler(async (req, res) => {
   const { idOne, idTwo } = req.params;
   const chat = await Chat
@@ -20,3 +31,13 @@ export const getChat = asyncHandler(async (req, res) => {
     chat,
   });
 });
+
+export const getRoomsUser = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const chat = await Chat.find({ $or: [{ sender: id }, { recipient: id }] });
+  const rooms = removeDuplicatedRoom(chat);
+  res.status(StatusCodes.OK).json({
+    status: 'success',
+    rooms: rooms.map((room) => room.room),
+  });
+})
--- routes/chat.route.js
+++ routes/chat.route.js
@@ -2,12 +2,18 @@ import { Router } from "express";
 const router = Router();
 
 import protectRoutes from "../middleware/auth.middleware.js";
-import { getChat } from "../controller/chat.controller.js";
+import { getChat, getRoomsUser } from "../controller/chat.controller.js";
 
 router.get(
   '/one/:idOne/:idTwo',
   protectRoutes,
   getChat
+)
+
+router.get(
+  '/rooms/:id',
+  protectRoutes,
+  getRoomsUser
 )
 
 export default router;
