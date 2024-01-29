bf26ff6db6dadc176aa95a98da56200c9e0ab4dc
mahmoud hamdy <72884628+m-hamdy1572@users.noreply.github.com>
2024-01-29 20:21:13 +0200

    launch project

--- test.js
+++
@@ -1,6 +0,0 @@
-import api from 'api';
-const sdk = api('@docs-tam/v1.0.0#4avjy1rlq4t0v5k');
-
-sdk.checkPaymentOptionsAvailability()
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
